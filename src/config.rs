f9d3f75311c55cc14ff5e1ea0b5e079cd62b7b76
Eugene Lebedev <duke.tougu@gmail.com>
2020-03-15 21:01:46 +0300

    [+] load telegram chat ids from config file

--- src/config.rs
+++ src/config.rs
@@ -7,10 +7,13 @@ pub mod config {
 
     const REST_AUTH_TOKEN_PROPERTY: &str = "rest-auth-token";
     const TELEGRAM_BOT_AUTH_TOKEN_PROPERTY: &str = "telegram-bot.auth-token";
+    const TELEGRAM_CHAT_IDS_PROPERTY: &str = "telegram-bot.chat-ids";
 
+    #[derive(Clone)]
     pub struct Config {
         pub rest_auth_token: String,
-        pub telegram_bot_token: String
+        pub telegram_bot_token: String,
+        pub telegram_chat_ids: Vec<i32>
     }
 
     pub fn load_from_file(file_name: &str) -> Result<Config, io::Error> {
@@ -21,9 +24,11 @@ pub mod config {
 
         let mut rest_auth_token: String = String::from("");
         let mut telegram_bot_auth_token: String = String::from("");
+        let mut telegram_chat_ids: Vec<i32> = Vec::new();
 
         let rest_auth_token_regex = get_property_regex(REST_AUTH_TOKEN_PROPERTY);
         let telegram_bot_auth_token_regex = get_property_regex(TELEGRAM_BOT_AUTH_TOKEN_PROPERTY);
+        let telegram_chat_ids_regex = get_property_regex(TELEGRAM_CHAT_IDS_PROPERTY);
 
         for line in buffered.lines() {
             let row = line.unwrap();
@@ -43,13 +48,31 @@ pub mod config {
                 telegram_bot_auth_token = String::from(&groups[1]);
                 info!("telegram bot auth token '{}'", telegram_bot_auth_token);
             }
+
+            if telegram_chat_ids_regex.is_match(&row) {
+                let groups_matches = telegram_chat_ids_regex.captures_iter(&row).next();
+                let groups = groups_matches.unwrap();
+
+                let telegram_chat_ids_row = String::from(&groups[1]);
+
+                let chat_ids: Vec<&str> = telegram_chat_ids_row.split("|").collect();
+
+                for chat_id in chat_ids {
+                    let chat_id_str: String = chat_id.to_string();
+                    let value: i32 = chat_id_str.parse::<i32>().unwrap();
+                    telegram_chat_ids.push(value);
+                }
+
+                info!("telegram chat ids '{}'", telegram_chat_ids_row);
+            }
         }
 
         if rest_auth_token != "" && telegram_bot_auth_token != "" {
             Ok(
                 Config {
                     rest_auth_token: String::from(rest_auth_token),
-                    telegram_bot_token: String::from(telegram_bot_auth_token)
+                    telegram_bot_token: String::from(telegram_bot_auth_token),
+                    telegram_chat_ids
                 }
             )
 
--- src/config_tests.rs
+++ src/config_tests.rs
@@ -1,14 +1,9 @@
 #[cfg(test)]
 mod config_tests {
-    use std::fs::File;
-    use std::path::Path;
-
     use crate::config::config::load_from_file;
 
     #[test]
     fn result_config_should_contain_rest_auth_token() {
-        let file = File::open("tests/telegram-bot.conf");
-
         match load_from_file("tests/telegram-bot.conf") {
             Ok(config) => {
                 assert_eq!(config.rest_auth_token, "53458092j45h9082j4gokjwlkfjg-23490gjq3049gj");
@@ -21,11 +16,24 @@ mod config_tests {
 
     #[test]
     fn result_config_should_contain_telegram_bot_auth_token() {
-        let file = File::open("tests/telegram-bot.conf");
-
         match load_from_file("tests/telegram-bot.conf") {
             Ok(config) => {
                 assert_eq!(config.telegram_bot_token, "AAAA-BBN23894gq034gadkgas");
+            }
+            Err(_error) => {
+                panic!("property value expected")
+            }
+        }
+    }
+
+    #[test]
+    fn result_config_should_contain_telegram_chat_ids() {
+        match load_from_file("tests/telegram-bot.conf") {
+            Ok(config) => {
+                let expected_chat_id1: i32 = 123456;
+                assert_eq!(config.telegram_chat_ids.contains(&expected_chat_id1), true);
+                let expected_chat_id2: i32 = 77712;
+                assert_eq!(config.telegram_chat_ids.contains(&expected_chat_id2), true);
             }
             Err(_error) => {
                 panic!("property value expected")
@@ -41,12 +49,18 @@ mod config_tests {
     #[test]
     #[should_panic]
     fn return_error_if_telegram_bot_auth_token_property_is_missing() {
-        load_from_file("tests/telegram-bot-missing-property1.conf");
+        assert_eq!(load_from_file("tests/telegram-bot-missing-property1.conf").is_err(), true);
     }
 
     #[test]
     #[should_panic]
     fn return_error_if_rest_auth_token_property_is_missing() {
-        load_from_file("tests/telegram-bot-missing-property2.conf");
+        assert_eq!(load_from_file("tests/telegram-bot-missing-property2.conf").is_err(), true);
+    }
+
+    #[test]
+    #[should_panic]
+    fn return_error_if_telegram_bot_chat_ids_is_missing() {
+        assert_eq!(load_from_file("tests/telegram-bot-missing-chat-ids.conf").is_err(), true);
     }
 }
